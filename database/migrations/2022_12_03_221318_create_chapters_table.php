3ae374d766a1d68abd0f2a54418854be0f7ea17d
feranmi <feranmiolasunkanmi91@gmail.com>
2022-12-03 23:20:28 +0100

    Working on new book and chapter migrations

---
+++ database/migrations/2022_12_03_221318_create_chapters_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChaptersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chapters', function (Blueprint $table) {
+            $table->id();
+            $table->string('chapter_number')->nullable();
+            $table->string('chapter_title')->nullable();
+            $table->longText('chapter_body')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chapters');
+    }
+}
